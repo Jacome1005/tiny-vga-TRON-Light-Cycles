049c3fc790cd028193b5c5eed948abb4d4b743bd
Jacobo Morales <j.morales1123@uniandes.edu.co>
2026-05-18 12:34:41 -0500

    Change MAX_LEN from 22 to 20

--- src/project.v
+++ src/project.v
@@ -39,7 +39,7 @@ module tt_um_tron_game  (
     localparam CELL_W    = 10;          // pixels per cell (in halved coords)
     localparam CELL_H    = 10;
 
-    localparam MAX_LEN   = 22;           // Trail length
+    localparam MAX_LEN   = 20;           // Trail length
     localparam LEN_BITS  = 5;           // ceil(log2(32))
 
     // Shared direction encoding
